c0a8a6660882b00f6c1ba85f7570c23fff7e243f
james victor <victorjames40v@gmail.com>
2024-10-15 23:52:41 +0100

    git fixed a width issue

--- src/components/Advert/Advert.jsx
+++ src/components/Advert/Advert.jsx
@@ -12,13 +12,13 @@ const Advert = () => {
        <div className='relative w-full h-[454.55px] rounded-[37.19px] border-[0.52px]  mt-7 custom-background bg-custom-overlay '>    
          <div className="absolute inset-0 bg-custom-overlay opacity-75">
              <div className="relative z-10">
-                  <div className='w-[379px] h-[339.75px] mx-3 md:mx-0 md:ml-[74.38px]  mt-[42.36px] flex md:justify-between flex-col'>
+                  <div className='w-full h-[339.75px] mx-3 md:mx-0 md:ml-[74.38px]  mt-[42.36px] flex md:justify-between flex-col'>
                        <div className='w-[173.56px] h-[16.53px] '>
                          <img src={loder} alt="loder" />
                        </div>
                        
-                       <div className='w-[379px] h-[149.66px] mt-24 md:mt-0'>
-                           <div className='w-[365px] h-[79.13px] text-white'>
+                       <div className='w-full  max-w-[379px] h-[149.66px] mt-24 md:mt-0'>
+                           <div className='w-full max-w-[365px] h-[79.13px] text-white'>
                               <h6 className='w-[230px] h-[25px] font-normal text-[20.66px] leading-[24.79px] '>
                                  TRENDING TOTEBAGS
                               </h6>
@@ -26,7 +26,7 @@ const Advert = () => {
                                  Denim Totebags
                               </h2>
                            </div>
-                           <p className='w-[379px]  h-[54px] font-medium leading-[27px] text-white'>Totebag’s made with jean fabric to meet your fashion taste</p>
+                           <p className='w-full max-w-[379px]  h-[54px] font-medium leading-[27px] text-white'>Totebag’s made with jean fabric to meet your fashion taste</p>
                        </div>
                   </div>
             </div>
